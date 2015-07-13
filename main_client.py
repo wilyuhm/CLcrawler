99b917318fa730e066965afafa31f5a12f60f64b
William Keidel <william.keidel@gmail.com>
2015-07-13 16:35:43 +0200

    A closer working version. Prints year, make, model, price if the post is formatted correctly

--- main_client.py
+++ main_client.py
@@ -28,5 +28,45 @@ HTMLfile.close()
 block_array = getHTMLblocks(s, '<p', '</p>') #contains html for individual postings
 
 #pass in HTML object code, find and match vehicle to a manufacturer and find the price
+#returns a tuple (vehicle, price)
+
 def findVehicleandPrice(text):
-	
+	manu_list = []
+	manufacturers = open('manufacturers.txt')	
+	manu_list = [line.rstrip('\n') for line in manufacturers]
+	manufacturers.close()
+
+	foundmake = 0
+	year = ''
+	make = ''
+	model = ''
+	price ='' 
+	prevword = text.split(' ')
+	for word in text.split(' '): #get make and model
+		if foundmake:
+			model = word.replace('</a>','')
+			break
+		for manu in manu_list:
+			if fuzz.ratio(word.lower(), manu) > 90:
+				foundmake = 1
+				make = manu
+				break
+		if foundmake==0:
+			prevword = word
+	if foundmake:
+		year = prevword.replace('class="hdrlnk">', '')
+
+	vehicle = year + ' ' + make.lower() + " " + model.lower()
+	priceindex = text.rfind('<span class="price">&#x0024;') + 28
+	if priceindex:
+		price = text[priceindex:text.find('<', priceindex, priceindex+7)]
+	if len(price) > 6:
+		price = '0'
+	return (vehicle, int(price))
+
+listings = []
+for block in block_array:
+	listings.append(findVehicleandPrice(block))
+for l in listings:
+	print l
+
--- request_html.py
+++ request_html.py
@@ -1,6 +1,9 @@
 #Grab HTML from given URL and send output to FILENAME
 
 import requests
+import time
+
+start = time.clock()
 
 URL = 'http://inlandempire.craigslist.org/search/cta'
 FILENAME = 'html.txt'
@@ -9,3 +12,6 @@ r = requests.get(URL)
 f = open(FILENAME, 'w')
 f.write(r.text.encode('utf-8'))
 f.close()
+
+end = time.clock()
+print 'Total elapsed time to get page: ' + str(end-start)
