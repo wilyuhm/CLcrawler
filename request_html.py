1b6cbe868b0a0b84b54507c90865b5fd6a5802ee
William Keidel <william.keidel@gmail.com>
2015-07-24 15:46:57 +0200

    Fetches 2500 results. Should do availability checking

--- request_html.py
+++ request_html.py
@@ -3,17 +3,34 @@
 import requests
 import time
 
-start = time.clock()
-
+start = time.time()
 URL = 'http://inlandempire.craigslist.org/search/cto?s='
 FILENAME = 'html.txt'
-NUMRESULTS = 100
+NUMRESULTS = 2400 #Max 2400
+NUMPAGES = NUMRESULTS/100 + 1
+#Craigslist allows a max of 2500 search results with 100 results per page
 
-r = requests.get(URL+str(NUMRESULTS-100))
-
+currpage=1
+print 'Getting page ' + str(currpage) + ' of ' + str(NUMPAGES) + '...'
+listingnumber = 0
+curr_URL = URL + str(listingnumber)
+r = requests.get(curr_URL)
+currpage+=1
 f = open(FILENAME, 'w')
 f.write(r.text.encode('utf-8'))
 f.close()
 
-end = time.clock()
-print 'Total elapsed time to get page: ' + str(end-start)
+f = open(FILENAME, 'a')
+listingnumber = 100
+if NUMRESULTS>2400:
+	NUMRESULTS=2400
+while(listingnumber <= NUMRESULTS):
+	curr_URL = URL + str(listingnumber)
+	print 'Getting page ' + str(currpage) + ' of ' + str(NUMPAGES) + '...'
+	currpage+=1
+	r = requests.get(curr_URL)
+	f.write(r.text.encode('utf-8'))
+	listingnumber = listingnumber + 100
+f.close()
+end = time.time()
+print 'Total elapsed time to get page(s): ' + str(end-start) 
